1fe92a73d31d6a70ab557d0ef761f72f8170ee2b
kcq <kechangqing@autohome.com.cn>
2020-07-25 14:10:45 +0800

    AlarmMapper重写为dynamic

--- frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/dao/mybatis/frostmourne/mapper/AlarmMapper.java
+++ frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/dao/mybatis/frostmourne/mapper/AlarmMapper.java
@@ -1,34 +1,9 @@
 package com.autohome.frostmourne.monitor.dao.mybatis.frostmourne.mapper;
 
-import java.util.Date;
-import java.util.List;
-
 import com.autohome.frostmourne.monitor.dao.mybatis.frostmourne.domain.Alarm;
-import org.apache.ibatis.annotations.Param;
 
 public interface AlarmMapper {
-    int deleteByPrimaryKey(Long id);
-
-    Long insert(Alarm record);
-
-    int insertSelective(Alarm record);
-
     Alarm selectByPrimaryKey(Long id);
-
-    int updateByPrimaryKeySelective(Alarm record);
-
-    int updateByPrimaryKey(Alarm record);
-
-    int updateStatus(@Param("alarmId") Long alarmId, @Param("status") String status);
-
-    List<Alarm> find(@Param("alarmId") Long alarmId, @Param("name") String name,
-                     @Param("teamName") String teamName, @Param("status") String status);
-
-    void updateAlarmLastExecuteInfo(@Param("alarmId") Long alarmId,
-                                    @Param("executeTime") Date executeTime,
-                                    @Param("executeResult") String executeResult);
-
-    int updateJobId(@Param("alarmId") Long alarmId, @Param("jobId") Long jobId);
 
     int total();
 }
