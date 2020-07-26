d0e420e9edd187545d708c53eff1b5096cb7abe1
kcq <kechangqing@autohome.com.cn>
2020-07-26 22:27:18 +0800

    镜像服务器替换为阿里云

---
+++ doc/docker/tag.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker tag local/frostmourne:0.3 registry.cn-hangzhou.aliyuncs.com/kechangqing/frostmourne:0.3
+docker tag local/frostmourne:0.3 registry.cn-hangzhou.aliyuncs.com/kechangqing/frostmourne:latest
