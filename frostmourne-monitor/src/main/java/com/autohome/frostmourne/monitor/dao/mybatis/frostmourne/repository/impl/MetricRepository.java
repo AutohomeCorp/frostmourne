1b4fe3e5477281ee9abad782511d72cc5ec23b39
kcq <kechangqing@autohome.com.cn>
2020-07-29 23:18:21 +0800

    数据库访问层重写

--- frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/dao/mybatis/frostmourne/repository/impl/MetricRepository.java
+++ frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/dao/mybatis/frostmourne/repository/impl/MetricRepository.java
@@ -49,21 +49,25 @@ public class MetricRepository implements IMetricRepository {
 
     @Override
     public int deleteByAlarm(Long alarmId) {
-        return metricDynamicMapper.delete(query -> query.where().and(MetricDynamicSqlSupport.alarm_id, isEqualTo(alarmId)));
+        return metricDynamicMapper.delete(query -> query.where()
+                .and(MetricDynamicSqlSupport.alarm_id, isEqualTo(alarmId)));
     }
 
     @Override
     public Optional<Metric> findOneByAlarm(Long alarmId) {
-        return metricDynamicMapper.selectOne(query -> query.where().and(MetricDynamicSqlSupport.alarm_id, isEqualTo(alarmId)));
+        return metricDynamicMapper.selectOne(query -> query.where()
+                .and(MetricDynamicSqlSupport.alarm_id, isEqualTo(alarmId)));
     }
 
     @Override
     public long datasourceCount(Long datasourceId) {
-        return metricDynamicMapper.count(query -> query.where().and(MetricDynamicSqlSupport.data_source_id, isEqualTo(datasourceId)));
+        return metricDynamicMapper.count(query -> query.where()
+                .and(MetricDynamicSqlSupport.data_source_id, isEqualTo(datasourceId)));
     }
 
     @Override
     public long datanameCount(Long datanameId) {
-        return metricDynamicMapper.count(query -> query.where().and(MetricDynamicSqlSupport.data_name_id, isEqualTo(datanameId)));
+        return metricDynamicMapper.count(query -> query.where()
+                .and(MetricDynamicSqlSupport.data_name_id, isEqualTo(datanameId)));
     }
 }
