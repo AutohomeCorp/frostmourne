6552cc09f947296bea8303d1503ec6a5e79afddf
kcq <kechangqing@autohome.com.cn>
2020-07-29 23:37:22 +0800

    数据库访问层重写

--- frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/dao/mybatis/frostmourne/mapper/RecipientMapper.java
+++ frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/dao/mybatis/frostmourne/mapper/RecipientMapper.java
@@ -1,24 +1,9 @@
 package com.autohome.frostmourne.monitor.dao.mybatis.frostmourne.mapper;
 
-import java.util.List;
-
 import com.autohome.frostmourne.monitor.dao.mybatis.frostmourne.domain.Recipient;
 
 public interface RecipientMapper {
-    Recipient selectByPrimaryKey(Long id);
-    /*int deleteByPrimaryKey(Long id);
-
-    int insert(Recipient record);
-
-    int insertSelective(Recipient record);
 
     Recipient selectByPrimaryKey(Long id);
 
-    int updateByPrimaryKeySelective(Recipient record);
-
-    int updateByPrimaryKey(Recipient record);
-
-    int deleteByAlarm(Long alarmId);
-
-    List<Recipient> findByAlarm(Long alarmId);*/
 }
