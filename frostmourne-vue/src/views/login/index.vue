3077b52f0d49722687500eb3328cd191174bde28
kechangqing <kechangqing@autohome.com.cn>
2020-07-29 17:30:30 +0800

    登录页面改进

--- frostmourne-vue/src/views/login/index.vue
+++ frostmourne-vue/src/views/login/index.vue
@@ -41,11 +41,6 @@
 
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
 
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
-
     </el-form>
   </div>
 </template>
