3b8f89091abad82aed3e70d652d3fe062d05c11a
kcq <kechangqing@autohome.com.cn>
2020-07-29 23:50:16 +0800

    数据库访问层重写

--- frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/dao/mybatis/frostmourne/mapper/RuleMapper.java
+++ frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/dao/mybatis/frostmourne/mapper/RuleMapper.java
@@ -3,6 +3,9 @@ package com.autohome.frostmourne.monitor.dao.mybatis.frostmourne.mapper;
 import com.autohome.frostmourne.monitor.dao.mybatis.frostmourne.domain.Rule;
 
 public interface RuleMapper {
+
+    Rule selectByPrimaryKey(Long id);
+
     /*int deleteByPrimaryKey(Long id);
 
     int insert(Rule record);
