952810a1bc69e1995b81a7d56dd62aabab7010db
kechangqing <kechangqing@autohome.com.cn>
2020-08-01 15:29:19 +0800

    数据库访问层重写

--- doc/docker/tag.sh
+++ doc/docker/tag.sh
@@ -2,3 +2,5 @@
 
 docker tag local/frostmourne:0.3 registry.cn-hangzhou.aliyuncs.com/kechangqing/frostmourne:0.3
 docker tag local/frostmourne:0.3 registry.cn-hangzhou.aliyuncs.com/kechangqing/frostmourne:latest
+docker tag local/frostmourne:0.3 frostmourne2020/frostmourne:0.3
+docker tag local/frostmourne:0.3 frostmourne2020/frostmourne:latest
