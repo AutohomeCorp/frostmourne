9285f1f0b93fe705a49949d6ffa1cdab53cbf672
kechangqing <kechangqing@autohome.com.cn>
2020-08-07 13:56:51 +0800

    增加用户角色关系表

--- doc/mysql-schema/tables/user_role.sql
+++ doc/mysql-schema/tables/user_role.sql
@@ -5,10 +5,11 @@ CREATE TABLE IF NOT EXISTS user_role
     account   VARCHAR(200) NOT NULL COMMENT '账号',
     role      VARCHAR(200) NOT NULL COMMENT '角色',
     creator   VARCHAR(200) NOT NULL COMMENT '创建人',
-    create_at DATETIME     NOT NULL COMMENT '创建时间',
-    modify_at DATETIME     NOT NULL COMMENT '修改时间',
-    modifier  VARCHAR(200) NOT NULL COMMENT '最后修改人'
+    create_at DATETIME     NOT NULL COMMENT '创建时间'
 )
     ENGINE = InnoDB
     DEFAULT CHARSET = utf8mb4
     COMMENT = '用户角色关系表';
+
+ALTER TABLE user_role
+    ADD INDEX idx_account (account);
