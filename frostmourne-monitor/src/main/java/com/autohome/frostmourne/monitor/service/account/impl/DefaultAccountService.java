d1666371d48088ec88468966704d57f409917d81
kechangqing <kechangqing@autohome.com.cn>
2020-08-18 18:51:59 +0800

    优化

--- frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/service/account/impl/DefaultAccountService.java
+++ frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/service/account/impl/DefaultAccountService.java
@@ -1,6 +1,5 @@
 package com.autohome.frostmourne.monitor.service.account.impl;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
