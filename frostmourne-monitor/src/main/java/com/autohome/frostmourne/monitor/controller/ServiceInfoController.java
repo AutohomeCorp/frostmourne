2b080ceb5ed06a9601f1c44767c82fd41746465b
kechangqing <kechangqing@autohome.com.cn>
2020-09-01 13:53:33 +0800

    增加服务管理功能

--- frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/controller/ServiceInfoController.java
+++ frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/controller/ServiceInfoController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = {"/serviceinfo"})
+@RequestMapping(value = {"/serviceinfo", "/api/monitor-api/serviceinfo"})
 public class ServiceInfoController {
 
     @Resource
