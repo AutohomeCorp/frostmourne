b5f3796519676eb7bf8af84a0e80ba8e76b0e8de
kechangqing <kechangqing@autohome.com.cn>
2020-09-03 17:05:13 +0800

    文档补充

--- frostmourne-vue/src/views/alarm/edit.vue
+++ frostmourne-vue/src/views/alarm/edit.vue
@@ -10,7 +10,7 @@
               </el-form-item>
             </el-col>
             <el-col :span="12">
-              <el-form-item label="状态:">
+              <el-form-item>
                 <el-switch v-model="form.status" active-value="OPEN" active-text="开启" inactive-value="CLOSE" inactive-text="关闭" />
               </el-form-item>
             </el-col>
