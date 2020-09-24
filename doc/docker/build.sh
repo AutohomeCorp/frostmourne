e0b2f76d121c9d93bda9737e15c1c0f9f432baa9
kechangqing <kechangqing@autohome.com.cn>
2020-09-24 19:34:32 +0800

    influxdb监控实现

--- doc/docker/build.sh
+++ doc/docker/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker rmi local/frostmourne:0.3
-docker build -t local/frostmourne:0.3 -f Dockerfile .
+docker rmi local/frostmourne:0.4
+docker build -t local/frostmourne:0.4 -f Dockerfile .
