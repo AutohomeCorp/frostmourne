60b89dcb44b6dac9ab90c45e29b838da2f9e616e
wangxiaoming <wangxiaoming@autohome.com.cn>
2020-11-30 11:03:14 +0800

    更新恢复通知SQL脚本

--- doc/mysql-schema/2020-11-29/change.sql
+++ doc/mysql-schema/2020-11-29/change.sql
@@ -1,3 +1,3 @@
 
 ALTER TABLE `alarm`
-  ADD COLUMN `recover_notice_status` tinyint NOT NULL DEFAULT '0' COMMENT '恢复通知开关，0:关,1:开';
+  ADD COLUMN `recover_notice_status` varchar(50) NOT NULL DEFAULT 'CLOSE' COMMENT '恢复通知开关（OPEN,CLOSE）';
