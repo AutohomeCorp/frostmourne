d0be71fc203ce518c326e5e3e07094d0e4cab78d
kechangqing <kechangqing@autohome.com.cn>
2021-05-30 19:31:06 +0800

    发版0.4-RELEASE
    发版0.4-RELEASE

--- doc/docker/tag.sh
+++ doc/docker/tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker tag local/frostmourne:0.4 registry.cn-hangzhou.aliyuncs.com/kechangqing/frostmourne:0.4
-docker tag local/frostmourne:0.4 registry.cn-hangzhou.aliyuncs.com/kechangqing/frostmourne:latest
-docker tag local/frostmourne:0.4 frostmourne2020/frostmourne:0.4
-docker tag local/frostmourne:0.4 frostmourne2020/frostmourne:latest
+docker tag local/frostmourne:0.5 registry.cn-hangzhou.aliyuncs.com/kechangqing/frostmourne:0.5
+docker tag local/frostmourne:0.5 registry.cn-hangzhou.aliyuncs.com/kechangqing/frostmourne:latest
+docker tag local/frostmourne:0.5 frostmourne2020/frostmourne:0.5
+docker tag local/frostmourne:0.5 frostmourne2020/frostmourne:latest
