0452f6cbc576a85255beb0f472b6ebba8ad21ef5
kechangqing <kechangqing@autohome.com.cn>
2022-04-07 10:54:50 +0800

    移除spi依赖

--- frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/model/message/MessageResult.java
+++ frostmourne-monitor/src/main/java/com/autohome/frostmourne/monitor/model/message/MessageResult.java
@@ -12,14 +12,32 @@ import lombok.Setter;
  * @author Aping
  * @since 2022/3/28 13:20
  */
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class MessageResult {
 
     private MessageWay way;
 
     private Boolean success;
 
+    public MessageResult() {}
+
+    public MessageResult(MessageWay messageWay, Boolean success) {
+        this.way = messageWay;
+        this.success = success;
+    }
+
+    public MessageWay getWay() {
+        return way;
+    }
+
+    public void setWay(MessageWay way) {
+        this.way = way;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
 }
