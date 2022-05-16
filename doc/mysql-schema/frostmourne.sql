1e1e5e20564bec3a7b5e52d7a17811a1b7d18675
kcq <kechangqing@autohome.com.cn>
2022-05-16 16:09:10 +0800

    Update frostmourne.sql

--- doc/mysql-schema/frostmourne.sql
+++ doc/mysql-schema/frostmourne.sql
@@ -428,7 +428,7 @@ CREATE TABLE IF NOT EXISTS alert_event
     alert_type VARCHAR(16) NOT NULL COMMENT '消息类型(问题报警: PROBLEM; 恢复通知: RECOVER)',
     in_silence TINYINT     NOT NULL COMMENT '是否在静默期',
     event_md5  JSON        NULL COMMENT '摘要md5',
-    upgrade    TINYINT     NOT NULL DEFAULT 0 COMMENT '报警升级'
+    upgrade    TINYINT     NOT NULL DEFAULT 0 COMMENT '报警升级',
     create_at  DATETIME    NOT NULL default CURRENT_TIMESTAMP COMMENT '创建时间',
     key idx_alarm_id (alarm_id),
     key idx_create_at (create_at)
